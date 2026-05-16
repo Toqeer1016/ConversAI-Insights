79089607766dab20042105f867cc7489f8eb90f9
Toqeer1016 <toqeerahmad1016@gmail.com>
2026-05-16 19:02:26 +0500

    updated project

--- conversai-frontend/src/services/uploadService.js
+++ conversai-frontend/src/services/uploadService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8080"; // your FastAPI backend
+const BASE_URL = "http://127.0.0.1:8000"; // your FastAPI backend
 
 export async function uploadChatFile(file) {
   const formData = new FormData();
